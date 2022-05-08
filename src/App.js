c87790ab161df57b80e91522fa9b70fd2a05d94b
“payal” <payalpayal@gmail.com>
2022-05-08 00:23:25 -0700

    d

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+
+import {  useDispatch, useSelector } from 'react-redux';
+import { myAsyncAction } from '.';
 import './App.css';
 
 function App() {
+
+      // state
+        let state =  useSelector((store)=>{ 
+            return store
+        });
+         let dispatch =useDispatch();
+      //function defination
+
+
+      //return statements
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <> 
+      {(state.name)}
+      <div>hello{console.log(state)}</div>
+      <button  onClick={()=>{dispatch(myAsyncAction())}}>button</button> 
+      </>
+
+  )
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,68 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { legacy_createStore as createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
+      let intialState = { 
+        name:'PAYAL',
+      }
+      let rootReducer = (prevState=intialState,action)=>{ 
+        let newState = prevState;
+
+      switch(action.type){ 
+        case 'CHANGE_TO_BHARAT':
+        console.log('CHANGETO BHARAT')
+        return{ 
+          ...newState,
+          name:action.payload
+        }
+      
+        
+        case 'CHANGE_TO_MADHU':
+        console.log('CHANGETOMADHU')
+        return{ 
+          ...newState,
+          name:action.payload
+        }
+      
+        default:
+    
+   return { 
+    ...newState,
+   } 
+ }
+ }
+ let actionCreator1 =()=>{ 
+  return { 
+    type:'CHANGE_TO_BHARAT',
+    payload:'BHARAT DHAKAD'
+}
+ }
+ export const  myAsyncAction  =()=> {
+    console.log('okoko');
+    return (dispatch)=>{ 
+          setTimeout(function(){ 
+            //console.log('okokok');
+         
+            dispatch(actionCreator1());
+            
+          },10000);
+    }
+ 
+}
+let store  = createStore(rootReducer,applyMiddleware(thunk));
+console.log(store);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+    
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+ 
